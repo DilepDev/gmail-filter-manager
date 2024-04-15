a34f552ad3771af3c880a4f80d2f7a5bc8158594
Dilep Dev <sdev.dev@gmail.com>
2024-04-15 20:00:45 +0530

    Add close methods to filter api calls

--- source/gmail/filters/filter_api.py
+++ source/gmail/filters/filter_api.py
@@ -12,7 +12,9 @@ class FilterAPI:
 
     def get_filter(self, userId, filter_id):
         try:
-            return self.gmail_filter_service.get(userId = userId, id = filter_id).execute()
+            filter = self.gmail_filter_service.get(userId = userId, id = filter_id).execute()
+            self.gmail.close()
+            return filter
         except HttpError:
             return None
         except Exception as error:
@@ -21,20 +23,26 @@ class FilterAPI:
     
     def create_filter(self, userId, filter):
         try:
-            return self.gmail_filter_service.create(userId = userId, body = filter).execute()
+            filter = self.gmail_filter_service.create(userId = userId, body = filter).execute()
+            self.gmail.close()
+            return filter
         except Exception as error:
             return error
 
 
     def get_all_filters(self, userId):
         try:
-            return self.gmail_filter_service.list(userId = userId).execute()
+            filters = self.gmail_filter_service.list(userId = userId).execute()
+            self.gmail.close()
+            return filters
         except Exception as error:
             return error
 
 
     def delete_filter(self, userId, filter_id):
         try:
-            return self.gmail_filter_service.delete(userId = userId, id = filter_id).execute()
+            filter = self.gmail_filter_service.delete(userId = userId, id = filter_id).execute()
+            self.gmail.close()
+            return filter
         except Exception as error:
             return error
